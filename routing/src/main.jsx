0bcadc818644417fd6fcad8c49572151c469b408
Hamza_Shafi <hamzashafi946@gmail.com>
2023-11-16 13:36:51 +0500

    new lines added

--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -4,7 +4,9 @@ import './index.css';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Github, {githubInfoLoader} from './components/Github/Github';
+import { Route, Router, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import User from './components/User/User';
 
 // const router = createBrowserRouter([
 //   {
@@ -26,6 +28,22 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 //           ]
 //   }
 // ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Router path='/' element={<Route />}>
+      <Routes path='' element={<Home />} />
+      <Routes path="about" element={<About />} />
+      <Routes path="contact" element={<Contact />} />
+      <Routes path='user/userID' element={<User/>}/>
+      <Routes
+      loader = {githubInfoLoader}
+      path = 'github'
+      element = {<Github/>}
+      />
+      </Router>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
