a1ca40e367d7e476f765fe1b210c90931923d2b0
Hamza_Shafi <hamzashafi946@gmail.com>
2023-11-16 11:47:54 +0500

    new line

--- routing/src/main.jsx
+++ routing/src/main.jsx
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.css';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <Layout/>, 
-    children: [
-      { path: "",
-       element: <Home />
-          },
-          { path: "about",
-          element: <About />
-          },
-          {
-            path: "contact",
-            element: <Contact />
-          }
-          ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/', 
+//     element: <Route/>, 
+//     children: [
+//       { 
+//         path: "",
+//        element: <Home/>
+//           },
+//           { 
+//             path: "about",
+//           element: <About/>
+//           },
+//           {
+//             path: "contact",
+//             element: <Contact/>
+//           }
+//           ]
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
